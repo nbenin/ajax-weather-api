30d32df8a5edfc17cfda66a19945cd795b87649a
Neil <neil.mf.benin@gmail.com>
2020-01-10 13:59:55 +0100

    trying again

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
-require(['script'], function() {
+(function () {
+
+    document.getElementById('Submit').addEventListener('click', function() {
+        queryWebsite().catch(error => {
+            console.log(error);
+        });
+    });
+
+})();
+
+async function queryWebsite() {
+
     const axios = require('axios');
 
-});
-
+    const response = await axios.get('api.openweathermap.org/data/2.5/forecast?id=524901&appid=1315d1ad0799dca3ca95161a1d5776e7')
+    console.log(response);
+}
