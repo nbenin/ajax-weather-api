a140d22e82b51a804d9492acab87d6902e881272
Neil <neil.mf.benin@gmail.com>
2020-01-10 16:23:31 +0100

    got the stupid thing, stupid type=submit on button

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
-//const myWeatherAppId = '315d1ad0799dca3ca95161a1d5776e7';
+// ASYNC FUNCTION LES GO
+(function () {
+    document.getElementById('submit').addEventListener('click', function() {
 
-// Make a request for a user with a given ID
-axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
-    .then(function (response) {
-// handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-// handle error
-        console.log(error);
-    })
-    .finally(function () {
-// always executed
+        // setting variables
+        const myWeatherAppId = '315d1ad0799dca3ca95161a1d5776e7';
+
+        axiosRequest().catch(error => {
+            console.log(error);
+        });
     });
+})();
+
+// async function for fetching url
+async function axiosRequest() {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto/');
+    console.log(response);
+}
 
 
 
