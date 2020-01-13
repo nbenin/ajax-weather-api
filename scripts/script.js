14c6071dbbbaf1aaacc661cf0c267c6521ba3a89
Neil <neil.mf.benin@gmail.com>
2020-01-13 11:55:29 +0100

    getting there, slowly but surely

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 (function () {
     document.getElementById('submit').addEventListener('click', function() {
 
-        // setting variables
+        // my Id, always the same
         const myWeatherAppId = '&APPID=1315d1ad0799dca3ca95161a1d5776e7&units=metric';
 
         axiosRequest(myWeatherAppId).catch(error => {
@@ -11,37 +11,70 @@
     });
 })();
 
+
+
 // async function for fetching url
 async function axiosRequest(myWeatherAppId) {
     const response = await axios.get('https://api.openweathermap.org/data/2.5/forecast?q=London,uk' + myWeatherAppId);
-    const currentFullDay = new Date();
+
     let arrayOfImportantInfo = splitObjectIntoFive(response.data);
 
-    let currentYear = currentFullDay.getFullYear();
-    let currentMonth = currentFullDay.getMonth();
-    let currentDate = currentFullDay.getDate();
-    let currentHours = currentFullDay.getHours();
-    let currentHourMultipleOfThree = Math.floor(currentHours / 3) * 3;
 
-    console.log(response.data, currentFullDay, currentYear, currentMonth, currentDate, currentHours, currentHourMultipleOfThree);
+    console.log(response.data, arrayOfImportantInfo);
 }
+
+
 
 // function to split object into 5 arrays
 function splitObjectIntoFive(weatherObj) {
 
-    let betterFormattedObj = [];
+    // set variables and empty array for a new object
+    let today = [], tomorrow = [], todayPlusTwo = [], todayPlusThree = [], todayPlusFour = [];
+    let betterFormattedObj = [today, tomorrow, todayPlusTwo, todayPlusThree, todayPlusFour];
+    let currentFullDay = new Date();
+    //let currentHourMultipleOfThree = Math.floor(currentHours / 3) * 3;
 
-    for (let x = 0; x > weatherObj.list.length; x++) {
-        if (weatherObj.list[x])
+    // looping through object and separating into 5 new objects with the info we need
+    for (let x = 0; x < weatherObj.list.length; x++) {
+
+        // compare dates, use difference factor to determine day, then organize
+        let comparableDate = new Date(weatherObj.list[x].dt * 1000);
+        let factorOfComparableDate = comparableDate.getDate() - currentFullDay.getDate();
+
+        switch (factorOfComparableDate) {
+            case 0:
+                today.push(weatherObj.list[x]);
+                break;
+            case 1:
+                tomorrow.push(weatherObj.list[x]);
+                break;
+            case 2:
+                todayPlusTwo.push(weatherObj.list[x]);
+                break;
+            case 3:
+                todayPlusThree.push(weatherObj.list[x]);
+                break;
+            case 4:
+                todayPlusFour.push(weatherObj.list[x]);
+                break;
+            case 5:
+                break;
+        }
     }
+    return betterFormattedObj;
 }
 
-// Class for new Weather Objects
-class weatherObject {
-    constructor(date, tempMax, tempMin, precipitation, wind, humidity) {
-        let newDate = this.date;
-        let tempMaximum = this.tempMax;
 
+
+// Class for new Weather Objects
+class WeatherObject {
+    constructor(newDate, newTempMax, newTempMin, newPrecipitation, newWind, newHumidity) {
+        this.date = newDate;
+        this.tempMax = newTempMax;
+        this.tempMin = newTempMin;
+        this.precipitation = newPrecipitation;
+        this.wind = newWind;
+        this.humidity = newHumidity;
     }
 }
 
